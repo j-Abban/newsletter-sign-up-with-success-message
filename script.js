0fc7e5f3de1d01987f67930b97f719195d98094c
j-Abban <justice.abban@amalitech.org>
2024-04-02 15:49:26 +0000

    event listeners

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
-const myform = document.querySelector("#myForm")
-const dissmiss = document.querySelector("#dissmiss")
-const email = document.querySelector("#email")
-const errorText = document.querySelector("#errorText")
-const container = document.querySelector("#.container")
-const successCard = document.querySelector(".success-card")
-const emailText = document.querySelector("#emailtext")
-const submitButton = document.querySelector(".submit-buttton")
+const myForm = document.querySelector("#myForm");
+const dissmiss = document.querySelector("#dissmiss");
+const email = document.querySelector("#email");
+const errorText = document.querySelector("#errorText");
+const container = document.querySelector(".container");
+const successCard = document.querySelector(".success-card");
+const emailText = document.querySelector("#emailtext");
+const submitButton = document.querySelector(".submit-button");
+
+myForm.addEventListener("submit", function(event){
+    event.preventDefault();
+
+    if (ValidateEmail(email.value)) {
+        submitButton.innerHTML = "Loading...";
+        email.disabled = true;
+        submitButton.disabled = true;
+
+        setTimeout(() => {
+            container.style.display = "none";
+            successCard.style.display = "block";
+            emailText.innerHTML = email.value;
+        }, 3000);
+    } else {
+        errorText.style.display = "block";
+        email.classList.add("error");
+    }
+});
